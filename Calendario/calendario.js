b70ad9c53b43f4a339d23814162af4b0afb36ab5
javierbolanosllano <javierbolanosllano@icloud.com>
2023-12-03 23:01:48 +0100

    terminando de actualizar el calendario

--- Calendario/calendario.js
+++ Calendario/calendario.js
@@ -133,3 +133,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
     cargarEventosGuardados();
 });
+
+document.getElementById('input').addEventListener('change', () => {
+    if (document.body.className.indexOf('dark') === -1) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  });
--- Calificaciones/calificaciones.js
+++ Calificaciones/calificaciones.js
@@ -25,3 +25,11 @@ document.addEventListener("DOMContentLoaded", function () {
         calificacionesBody.appendChild(row);
     });
 });
+
+document.getElementById('input').addEventListener('change', () => {
+    if (document.body.className.indexOf('dark') === -1) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.remove('dark');
+    }
+  });
