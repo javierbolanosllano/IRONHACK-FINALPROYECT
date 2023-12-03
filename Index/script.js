1a56b596fd05a0afb87df036d05d647297af3d0a
javierbolanosllano <javierbolanosllano@icloud.com>
2023-12-03 23:48:42 +0100

    index corregido

--- Index/script.js
+++ Index/script.js
@@ -1,13 +1,22 @@
-document.getElementById('input').addEventListener('change', () => {
+document.addEventListener('DOMContentLoaded', function () {
+  // Wait for the DOM to be fully loaded before accessing elements
+
+  document.getElementById('input').addEventListener('change', () => {
     if (document.body.className.indexOf('dark') === -1) {
       document.body.classList.add('dark');
     } else {
       document.body.classList.remove('dark');
     }
   });
-  
-  document.getElementById('login-btn').addEventListener('click', () => {
-    const loginForm = document.getElementById('login-form');
+
+  const loginBtn = document.getElementById('login-btn');
+  const loginForm = document.getElementById('login-form');
+
+  // Hide the login form initially
+  loginForm.style.display = 'none';
+
+  loginBtn.addEventListener('click', () => {
+    // Toggle the display property of the login form
     loginForm.style.display = loginForm.style.display === 'none' ? 'flex' : 'none';
   });
-  
+});
